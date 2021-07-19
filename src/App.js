9737b4f880a495eed75e05a8d2c7c05947bd7ea9
EthanPrintz <ethanprintz@gmail.com>
2021-07-18 20:56:29 -0400

    Data querying working now!

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { readString } from 'react-papaparse'
-// import 
-
-const parameterTypes = [ "um025", "pm25" ];
-const sensors = []
 
 function getSensorDataByURL(url){
   const response = fetch(url)
@@ -19,13 +15,22 @@ function getSensorDataByURL(url){
     const sensorData = arr.data.slice(1)
     let readings = [];
     const data = sensorData.slice(1)
-    // parameterTypes.forEach((parameter) => {
-    // readings.push({parameter, data: data.filter(dataEntry => dataEntry[6] === parameter).map(data => [data[4], data[7]])})
-    readings.push({
-      // utc: data,
-      // um025: ,
-      // pm25: 
-    })
+    data.forEach(reading => {
+      const utcTimestamp = reading[4];
+      const readingType = reading[6];
+      const readingValue = reading[7];
+
+      if(readings.some(r => r.utc === utcTimestamp)){
+        readings[readings.findIndex(r => r.utc === utcTimestamp)][readingType] = readingValue
+      } else{
+        readings.push({
+          utc: utcTimestamp,
+          [readingType]: readingValue
+        })
+      }
+
+    });
+    
     // })
 
     const returnData = {
@@ -36,7 +41,7 @@ function getSensorDataByURL(url){
       readings
     }
 
-    console.log('Sensor Data:',returnData)
+    console.log(returnData)
     return returnData;
   });
 }
@@ -45,23 +50,7 @@ function App() {
 
   useEffect(() => {   
     getSensorDataByURL('https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?format=csv&limit=66536&location=224746&date_from=2021-04-17T07%3A00%3A00.000Z&date_to=2021-05-01T07%3A00%3A00.000Z&parameter=130&parameter=2')
-  })
-
-  /*
-    {
-      locationid: int
-      locationString: string
-      lat: float
-      long: float
-      readings: [
-        {
-          utc: Date,
-          pm25: float
-          um025: float
-        }
-      ]
-    }
-   */
+  }, [])
 
   return (
     <div className="App">
