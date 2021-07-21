8e9e2e3f14c8d32de4ab13df7a35e5291604a953
Ethan Printz <ethanprintz@gmail.com>
2021-07-21 10:40:59 -0400

    Started work on layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import './App.css';
-import { readString } from 'react-papaparse'
+import { readString } from 'react-papaparse';
 
-function getSensorDataByURL(url){
+function getSensorDataByURL(url) {
   const response = fetch(url)
-  .then(response => response.text())
-  .then(csv => readString(csv))
-  .catch(err => console.log(err))
-  
-  response.then(arr => {      
+    .then((response) => response.text())
+    .then((csv) => readString(csv))
+    .catch((err) => console.log(err));
+
+  response.then((arr) => {
     // locationId, location, city, coutnry, utc, local, parameter, value, unit, latitutde, longitude
     // 0         , 1       , 2   , 3      , 4  , 5    , 6        , 7    , 8   , 9        , 10
-    const sensorData = arr.data.slice(1)
+    const sensorData = arr.data.slice(1);
     let readings = [];
-    const data = sensorData.slice(1)
-    data.forEach(reading => {
+    const data = sensorData.slice(1);
+    data.forEach((reading) => {
       const utcTimestamp = reading[4];
       const readingType = reading[6];
       const readingValue = reading[7];
 
-      if(readings.some(r => r.utc === utcTimestamp)){
-        readings[readings.findIndex(r => r.utc === utcTimestamp)][readingType] = readingValue
-      } else{
+      if (readings.some((r) => r.utc === utcTimestamp)) {
+        readings[readings.findIndex((r) => r.utc === utcTimestamp)][
+          readingType
+        ] = readingValue;
+      } else {
         readings.push({
           utc: utcTimestamp,
-          [readingType]: readingValue
-        })
+          [readingType]: readingValue,
+        });
       }
-
     });
-    
+
     // })
 
     const returnData = {
@@ -38,38 +39,44 @@ function getSensorDataByURL(url){
       locationString: sensorData[1][1],
       latitude: sensorData[1][9],
       longitutde: sensorData[1][10],
-      readings
-    }
+      readings,
+    };
 
-    console.log(returnData)
+    console.log(returnData);
     return returnData;
   });
 }
 
 function App() {
-
-  useEffect(() => {   
-    getSensorDataByURL('https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?format=csv&limit=66536&location=224746&date_from=2021-04-17T07%3A00%3A00.000Z&date_to=2021-05-01T07%3A00%3A00.000Z&parameter=130&parameter=2')
-  }, [])
+  useEffect(() => {
+    getSensorDataByURL(
+      'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?format=csv&limit=66536&location=224746&date_from=2021-04-17T07%3A00%3A00.000Z&date_to=2021-05-01T07%3A00%3A00.000Z&parameter=130&parameter=2'
+    );
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <LeftCol></LeftCol>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  background-color: grey;
+`;
+
+const LeftCol = styled.div`
+  width: 20rem;
+  height: 90vh;
+  position: absolute;
+  top: 5vh;
+  left: 5vh;
+  border-radius: 2rem;
+  background-color: #f4f4f4;
+`;
 
 export default App;
