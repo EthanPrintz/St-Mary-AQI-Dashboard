ce8580dd76d37ca88d0317406812dc6651e54b22
Ethan Printz <ethanprintz@gmail.com>
2021-07-21 13:08:02 -0400

    Split getSensorData into seperate util file

--- src/App.js
+++ src/App.js
@@ -1,52 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import './App.css';
-import { readString } from 'react-papaparse';
 import LeftCol from './components/LeftCol';
-
-function getSensorDataByURL(url) {
-  const response = fetch(url)
-    .then((response) => response.text())
-    .then((csv) => readString(csv))
-    .catch((err) => console.log(err));
-
-  response.then((arr) => {
-    // locationId, location, city, coutnry, utc, local, parameter, value, unit, latitutde, longitude
-    // 0         , 1       , 2   , 3      , 4  , 5    , 6        , 7    , 8   , 9        , 10
-    const sensorData = arr.data.slice(1);
-    let readings = [];
-    const data = sensorData.slice(1);
-    data.forEach((reading) => {
-      const utcTimestamp = reading[4];
-      const readingType = reading[6];
-      const readingValue = reading[7];
-
-      if (readings.some((r) => r.utc === utcTimestamp)) {
-        readings[readings.findIndex((r) => r.utc === utcTimestamp)][
-          readingType
-        ] = readingValue;
-      } else {
-        readings.push({
-          utc: utcTimestamp,
-          [readingType]: readingValue,
-        });
-      }
-    });
-
-    // })
-
-    const returnData = {
-      locationId: sensorData[1][0],
-      locationString: sensorData[1][1],
-      latitude: sensorData[1][9],
-      longitutde: sensorData[1][10],
-      readings,
-    };
-
-    console.log(returnData);
-    return returnData;
-  });
-}
+import { getSensorDataByURL } from './utils/getSensorData';
+import './App.css';
 
 function App() {
   useEffect(() => {
---
+++ src/utils/getSensorData.js
@@ -0,0 +1,47 @@
+import { readString } from 'react-papaparse';
+
+/*
+// locationId, location, city, coutnry, utc, local, parameter, value, unit, latitutde, longitude
+// 0         , 1       , 2   , 3      , 4  , 5    , 6        , 7    , 8   , 9        , 10
+*/
+export const getSensorDataByURL = (url) => {
+  const response = fetch(url)
+    .then((res) => res.text())
+    .then((csv) => readString(csv))
+    .catch((err) => console.log(err));
+
+  response.then((arr) => {
+    const sensorData = arr.data.slice(1);
+    let readings = [];
+    const data = sensorData.slice(1);
+    data.forEach((reading) => {
+      const utcTimestamp = reading[4];
+      const readingType = reading[6];
+      const readingValue = reading[7];
+
+      if (readings.some((r) => r.utc === utcTimestamp)) {
+        readings[readings.findIndex((r) => r.utc === utcTimestamp)][
+          readingType
+        ] = readingValue;
+      } else {
+        readings.push({
+          utc: utcTimestamp,
+          [readingType]: readingValue,
+        });
+      }
+    });
+
+    // })
+
+    const returnData = {
+      locationId: sensorData[1][0],
+      locationString: sensorData[1][1],
+      latitude: sensorData[1][9],
+      longitutde: sensorData[1][10],
+      readings,
+    };
+
+    console.log(returnData);
+    return returnData;
+  });
+};
