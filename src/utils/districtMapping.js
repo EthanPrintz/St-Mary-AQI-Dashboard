7047e177defbf58d792d2f08e81f1fbecb3a7f95
Kingsley Szeto <szeto.kingsley@gmail.com>
2021-07-28 23:43:09 -0400

    sensors but better indentation

--- src/utils/districtMapping.js
+++ src/utils/districtMapping.js
@@ -6,127 +6,127 @@ export const DISTRICT_MAPPING = {
             'sensors': [92271, 91277, 92885]
         },
         {
-           'abbreviation': 'res',
+            'abbreviation': 'res',
             'name': 'Ridge Elementary School',
             'sensors': [92793, 91809, 90635]
         },
         {
-           'abbreviation': 'tces',
+            'abbreviation': 'tces',
             'name': 'Town Creek Elementary School',
             'sensors': [90599, 90655, 92291, 92803, 91811, 91819]
         },
         {
-           'abbreviation': 'srms',
+            'abbreviation': 'srms',
             'name': 'Spring Ridge Middle School',
             'sensors': [91347, 92789, 92821]
         },
         {
-           'abbreviation': 'phes',
+            'abbreviation': 'phes',
             'name': 'Park Hall Elementary School',
             'sensors': [92881, 91393, 92859]
         },
         {
-           'abbreviation': 'gwces',
+            'abbreviation': 'gwces',
             'name': 'George Washington Carver Elementary School',
             'sensors': [93019, 90589, 85659]
         },
         {
-           'abbreviation': 'gmhs',
+            'abbreviation': 'gmhs',
             'name': 'Great Mills High School',
             'sensors': [90817]
         },
         {
-           'abbreviation': 'fa',
+            'abbreviation': 'fa',
             'name': 'Fairlead Academy',
             'sensors': [92289, 93001, 92285]
         },
         {
-           'abbreviation': 'lpes',
+            'abbreviation': 'lpes',
             'name': 'Lexington Park Elementary School',
             'sensors': [92281, 90881, 92245]
         },
         {
-           'abbreviation': 'gksac',
+            'abbreviation': 'gksac',
             'name': 'Greenview Knolls School Age Center',
             'sensors': [90647, 92559, 92325]
         },
         {
-           'abbreviation': 'ems',
+            'abbreviation': 'ems',
             'name': 'Esperanza Middle School',
             'sensors': [92969, 83993, 92689]
         },
         {
-           'abbreviation': 'ghes',
+            'abbreviation': 'ghes',
             'name': 'Green Holly Elementary School',
             'sensors': [90687, 92701, 85473]
         },
         {
-           'abbreviation': 'ees',
+            'abbreviation': 'ees',
             'name': 'Evergreen Elementary School',
             'sensors': [92437, 92427]
         },
         {
-           'abbreviation': 'hes',
+            'abbreviation': 'hes',
             'name': 'Hollywood Elementary School',
             'sensors': [92833, 90849, 85455]
         },
         {
-           'abbreviation': 'lhs',
+            'abbreviation': 'lhs',
             'name': 'Leonardtown High School',
             'sensors': [92769, 92695, 92683]
         },
         {
-           'abbreviation': 'lms',
+            'abbreviation': 'lms',
             'name': 'Leonardtown Middle School',
             'sensors': [92855, 92415, 93017]
         },
         {
-           'abbreviation': 'les',
+            'abbreviation': 'les',
             'name': 'Leonardtown Elementary School',
             'sensors': [90645, 92745, 83855]
         },
         {
-           'abbreviation': 'cwfdes',
+            'abbreviation': 'cwfdes',
             'name': 'Captain Walter Francis Duke Elementary School',
             'sensors': [93027, 90659]
         },
         {
-           'abbreviation': 'bbes',
+            'abbreviation': 'bbes',
             'name': 'Benjamin Banneker Elementary School',
             'sensors': [93015, 93025, 93013]
         },
         {
-           'abbreviation': 'des',
+            'abbreviation': 'des',
             'name': 'Dynard Elementary School',
             'sensors': [92421, 92425, 93021]
         },
         {
-           'abbreviation': 'chs',
+            'abbreviation': 'chs',
             'name': 'Chopticon High School',
             'sensors': [90629, 91329, 90641]
         },
         {
-           'abbreviation': 'mbms',
+            'abbreviation': 'mbms',
             'name': 'Margaret Brent Middle School',
             'sensors': [92883, 92827]
         },
         {
-           'abbreviation': 'oes',
+            'abbreviation': 'oes',
             'name': 'Oakville Elementary School',
             'sensors': [89235, 92279, 92795]
         },
         {
-           'abbreviation': 'mes',
+            'abbreviation': 'mes',
             'name': 'Mechanicsville Elementary School',
             'sensors': [92679, 92999, 92687]
         },
         {
-           'abbreviation': 'wmes',
+            'abbreviation': 'wmes',
             'name': 'White Marsh Elementary School',
             'sensors': [92747, 92785, 91383]
         },
         {
-           'abbreviation': 'jlrs',
+            'abbreviation': 'jlrs',
             'name': 'Jamie L. Roberts Stadium',
             'sensors': [92807]
         }
