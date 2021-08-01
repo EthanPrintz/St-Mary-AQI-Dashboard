875a6c927ff39cf23266e57a13ce82bea1eef511
Kingsley Szeto <szeto.kingsley@gmail.com>
2021-08-01 18:30:11 -0400

    dynamic add bar charts, edit props for barchart component

--- src/components/RightModal/BarViewController.jsx
+++ src/components/RightModal/BarViewController.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
+import BarChart from "./BarChart";
 import HParamSelector from "./HParamSelector";
+
+let GRAPH_DIMENSIONS = [
+  window.screen.width * 0.24,
+  window.screen.width * 0.07,
+  window.screen.width * 0.01,
+];
 
 function BarViewController() {
   const [timespanState, setTimespanState] = useState([
@@ -16,6 +23,43 @@ function BarViewController() {
     { value: "Humidity", checked: false },
   ]);
 
+  const [displayedDataSets, setDisplayedDatasets] = useState({
+    mode: "24 Hours",
+    graphs: {
+      "PM 2.5": {
+        data: [50, 70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10],
+      },
+      "PM 10": {
+        data: [50, 70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10],
+      },
+      Temperature: {
+        data: [50, 70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10],
+      },
+      Humidity: {
+        data: [50, 70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10],
+      },
+    },
+  });
+
+  function renderGraphs() {
+    const barChartComponents = [];
+    airFactors.forEach((airFactor) => {
+      if (airFactor.checked) {
+        console.log(airFactor.value)
+        barChartComponents.push(
+          <BarChart
+            className="hidden"
+            data={displayedDataSets.graphs[airFactor.value].data}
+            dimensions={GRAPH_DIMENSIONS}
+            desc={airFactor.value}
+            gray={["Temperature", "Humidity"].includes(airFactor.value)}
+          />
+        );
+      }
+    });
+    return barChartComponents;
+  }
+
   return (
     <>
       <HParamSelector
@@ -30,6 +74,7 @@ function BarViewController() {
         setDataState={setAirFactors}
         allowMultiple
       />
+      <div className="bar-container">{renderGraphs()}</div>
     </>
   );
 }
