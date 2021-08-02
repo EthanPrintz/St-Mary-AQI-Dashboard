bc541b56b6d5dcd25ed71f087185d62f14edaca3
Kingsley Szeto <szeto.kingsley@gmail.com>
2021-08-01 21:53:10 -0400

    indentation

--- src/App.js
+++ src/App.js
@@ -1,90 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import LeftCol from './components/LeftCol';
-import RightModal from './components/RightModal';
-import Map from './components/Map';
-import BarChart from './components/BarChart';
-import SensorCurrentDisplay from './components/SensorCurrentDisplay';
-import CSVModal from './components/CSVModal';
-import { getSensorDataByURL } from './utils/getSensorData';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import LeftCol from "./components/LeftCol";
+import RightModal from "./components/RightModal";
+import Map from "./components/Map";
+import BarChart from "./components/BarChart";
+import SensorCurrentDisplay from "./components/SensorCurrentDisplay";
+import CSVModal from "./components/CSVModal";
+import { getSensorDataByURL } from "./utils/getSensorData";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  let date_range = ["24 hours", "3 days", "1 week", "2 weeks"];
 
-  let date_range = ['24 hours', '3 days', '1 week', '2 weeks'];
+  let graph_names = ["PM 25", "PM 10", "Temperature", "Humidity"];
 
-  let graph_names = ['PM 25', 'PM 10', 'Temperature', 'Humidity'];
+  const [sensorAvg, getSensorAvg] = useState([0, 0, 0]);
 
-    const [sensorAvg, getSensorAvg] = useState([0, 0, 0]);
+  const [pm25data, setpm25Data] = useState([]);
 
-    const [pm25data, setpm25Data] = useState([]);
+  const [pm10data, setpm10Data] = useState([]);
+  async function updateSensorData() {
+    const BASE_URL =
+      "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?format=json&";
 
-    const [pm10data, setpm10Data] = useState([]);
-    async function updateSensorData() {
-		const BASE_URL = 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?format=json&';
+    const queries = [];
+    const selectedSensors = ["224756"];
+    const TODAY = new Date();
+    const selectedFirstDate = JSON.stringify(TODAY.getDate() - 1).replaceAll(
+      '"',
+      ""
+    );
+    const selectedLastDate = JSON.stringify(TODAY).replaceAll('"', "");
+    const selectedAirFactors = graph_names.map((item) =>
+      item.toLowerCase().replace(/\s/g, "")
+    );
 
-		const queries = [];
-		const selectedSensors = ['224756'];
-		const TODAY = new Date()
-		const selectedFirstDate = JSON.stringify(TODAY.getDate() - 1).replaceAll('"', '');
-		const selectedLastDate = JSON.stringify(TODAY).replaceAll('"', '');
-		const selectedAirFactors = graph_names.map((item) => item.toLowerCase().replace(/\s/g, ''));;
+    // need to get sensorID, not name
+    selectedSensors.forEach((sensor) => {
+      const parameters = [];
+      parameters.push(`location=${sensor}`);
+      selectedAirFactors.forEach((airFactor) => {
+        parameters.push(`parameter=${airFactor}`);
+      });
+      parameters.push(`date_from=${selectedFirstDate}`);
+      parameters.push(`date_to=${selectedLastDate}`);
 
-		// need to get sensorID, not name
-		selectedSensors.forEach((sensor) => {
-		  const parameters = [];
-		  parameters.push(`location=${sensor}`);
-		  selectedAirFactors.forEach((airFactor) => {
-			parameters.push(`parameter=${airFactor}`);
-		  });
-		  parameters.push(`date_from=${selectedFirstDate}`);
-		  parameters.push(`date_to=${selectedLastDate}`);
+      queries.push(`${BASE_URL}${parameters.join("&")}`);
+    });
 
-		  queries.push(`${BASE_URL}${parameters.join('&')}`);
-		});
+    console.log(queries);
 
-		console.log(queries);
+    const response = axios.get(queries[0]);
 
-		const response = axios.get(queries[0]);
+    response.then((raw) => {
+      let data = raw.data.results;
 
-		response.then((raw)=>{
-			let data = raw.data.results
+      //pm25
+      let pm25 = data
+        .filter((item) => item.parameter === "pm25")
+        .map((item) => 10 * item.value);
+      //pm10
+      let pm10 = data
+        .filter((item) => item.parameter === "pm10")
+        .map((item) => 10 * item.value);
 
-			//pm25
-			let pm25 = data.filter((item) => item.parameter == "pm25").map((item) => 10* item.value)
-			//pm10
-			let pm10 = data.filter((item) => item.parameter == "pm10").map((item) => 10* item.value)
+      setpm25Data(pm25);
+      setpm10Data(pm10);
 
-			setpm25Data(pm25);
-			setpm10Data(pm10);
-
-			return [pm25, pm10];
-		})
-
-	  }
-
+      return [pm25, pm10];
+    });
+  }
 
   useEffect(() => {
     //console.log(getSensorDataByURL());
 
-    document.getElementById('date-selector-button-0').classList.add('selected');
+    document.getElementById("date-selector-button-0").classList.add("selected");
     document
-      .getElementById('graph-selector-button-0')
-      .classList.add('selected');
+      .getElementById("graph-selector-button-0")
+      .classList.add("selected");
     document
-      .getElementById('graph-selector-button-1')
-      .classList.add('selected');
+      .getElementById("graph-selector-button-1")
+      .classList.add("selected");
     document
-      .getElementById('graph-selector-button-2')
-      .classList.add('selected');
+      .getElementById("graph-selector-button-2")
+      .classList.add("selected");
     //const data = getSensorDataByURL(
     //  'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?format=csv&limit=66536&location=224746&date_from=2021-04-17T07%3A00%3A00.000Z&date_to=2021-05-01T07%3A00%3A00.000Z&parameter=130&parameter=2'
     //);
-	updateSensorData()
-
+    updateSensorData();
   }, []);
-
 
   const [data, setData] = useState([
     50, 70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10,
@@ -103,29 +108,28 @@ function App() {
     window.screen.width * 0.01,
   ];
 
-
   function updateDateRange(index) {
     for (let i = 0; i < 4; i++) {
-      if (index == i) {
+      if (index === i) {
         document
-          .getElementById('date-selector-button-' + i)
-          .classList.add('selected');
+          .getElementById("date-selector-button-" + i)
+          .classList.add("selected");
       } else {
         document
-          .getElementById('date-selector-button-' + i)
-          .classList.remove('selected');
+          .getElementById("date-selector-button-" + i)
+          .classList.remove("selected");
       }
     }
-    if (index == 0) {
+    if (index === 0) {
       setData([50, 70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10]);
     }
-    if (index == 1) {
+    if (index === 1) {
       setData([70, 20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10, 50]);
     }
-    if (index == 2) {
+    if (index === 2) {
       setData([20, 90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10, 50, 80]);
     }
-    if (index == 3) {
+    if (index === 3) {
       setData([90, 70, 30, 80, 40, 20, 50, 30, 90, 20, 10, 50, 80, 20]);
     }
   }
@@ -133,12 +137,12 @@ function App() {
   function updateGraphSelection(index) {
     if (graphs[index]) {
       document
-        .getElementById('graph-selector-button-' + index)
-        .classList.remove('selected');
+        .getElementById("graph-selector-button-" + index)
+        .classList.remove("selected");
     } else {
       document
-        .getElementById('graph-selector-button-' + index)
-        .classList.add('selected');
+        .getElementById("graph-selector-button-" + index)
+        .classList.add("selected");
     }
     let temp = [...graphs];
     temp[index] = !temp[index];
@@ -147,8 +151,8 @@ function App() {
 
   return (
     <AppContainer>
-	  {/*<LeftCol />*/}
-	  {/*<Map />*/}
+      {/*<LeftCol />*/}
+      {/*<Map />*/}
       <RightModal>
         <SensorCurrentDisplay
           dimensions={sensordimensions}
@@ -156,29 +160,29 @@ function App() {
           front={47}
           backright={47}
         />
-        See Data For:{' '}
+        See Data For:{" "}
         {date_range.map((date, i) => (
           <button
             className="selector-button"
-            id={'date-selector-button-' + i}
+            id={"date-selector-button-" + i}
             key={i}
             onClick={() => updateDateRange(i)}
           >
-            {' '}
-            {date}{' '}
+            {" "}
+            {date}{" "}
           </button>
         ))}
         <br />
-        Toggle Data:{' '}
+        Toggle Data:{" "}
         {graph_names.map((graph_name, i) => (
           <button
-            className={'selector-button'}
-            id={'graph-selector-button-' + i}
+            className={"selector-button"}
+            id={"graph-selector-button-" + i}
             key={i}
             onClick={() => updateGraphSelection(i)}
           >
-            {' '}
-            {graph_name}{' '}
+            {" "}
+            {graph_name}{" "}
           </button>
         ))}
         {graphs[0] && (
