9e16ae3d633ff31b7e220ec7ec491a0145c385f6
Ethan Printz <ethanprintz@gmail.com>
2021-08-07 13:07:21 -0400

    Correct school id querying

--- src/components/RightModal/index.jsx
+++ src/components/RightModal/index.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from "react";
-import { ReactComponent as BuilderSVG } from "../../assets/icons/query-builder.svg";
-import { ReactComponent as GraphViewSVG } from "../../assets/icons/graph-view.svg";
-import LocationTitle from "./LocationTitle";
-import "./style.scss";
+import React, { useState } from 'react';
+import { ReactComponent as BuilderSVG } from '../../assets/icons/query-builder.svg';
+import { ReactComponent as GraphViewSVG } from '../../assets/icons/graph-view.svg';
+import LocationTitle from './LocationTitle';
+import './style.scss';
 
-import DataViewer from "./DataViewer";
-import CSVModal from "./CSVModal";
-import SwitchSection from "./SwitchSection";
+import DataViewer from './DataViewer';
+import CSVModal from './CSVModal';
+import SwitchSection from './SwitchSection';
 
 function RightModal(props) {
   const [showDataCharts, setShowDataCharts] = useState(true);
-  console.log(props.combinedData)
+  console.log(props.combinedData);
 
   return (
     <div className="right-modal">
       {/* TODO: remove hard-coded school loc */}
-      <LocationTitle location="gmhs" schoolName = {props.combinedData.length !== 0 ? props.combinedData[props.selectedSchoolID].name : "School Name"}
-        liveAQI={props.combinedData.length !== 0 ? props.combinedData[props.selectedSchoolID].liveAQI : 0}/>
-      {showDataCharts ? <DataViewer combinedData={props.combinedData} selectedSchoolID={props.selectedSchoolID}/> : <CSVModal school="gmhs" />}
+      <LocationTitle
+        location="gmhs"
+        schoolName={
+          props.combinedData.length !== 0
+            ? props.combinedData.find(
+                (school) => school.id === props.selectedSchoolID
+              )?.name
+            : 'School Name'
+        }
+        liveAQI={
+          props.combinedData.length !== 0
+            ? props.combinedData.find(
+                (school) => school.id === props.selectedSchoolID
+              )?.liveAQI
+            : 0
+        }
+      />
+      {showDataCharts ? (
+        <DataViewer
+          combinedData={props.combinedData}
+          selectedSchoolID={props.selectedSchoolID}
+        />
+      ) : (
+        <CSVModal school="gmhs" />
+      )}
       <SwitchSection
         svg={showDataCharts ? <BuilderSVG /> : <GraphViewSVG />}
         text={
-          showDataCharts ? "Open Data Query Builder" : "Return to Graph View"
+          showDataCharts ? 'Open Data Query Builder' : 'Return to Graph View'
         }
         onClick={(e) => {
           setShowDataCharts(!showDataCharts);
